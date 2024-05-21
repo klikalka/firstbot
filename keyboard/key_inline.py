87daf75fc2968f81f2c10298e75c79d176bf78f2
Cybersport <Cybersport@26kadr.internal>
2024-05-21 13:28:32 +0300

    3 задание

--- keyboard/key_inline.py
+++ keyboard/key_inline.py
@@ -12,3 +12,27 @@ def get_key_inline2():
     but_inline4 = InlineKeyboardButton('Трешовая история о которой тяжело молчать', url='https://www.techinsider.ru/science/821283-homyaki-pitanie-privychki-i-drugie-interesnye-fakty/#:~:text=%D0%A1%D0%BE%D0%B3%D0%BB%D0%B0%D1%81%D0%BD%D0%BE%20ASPCA%2C%20%D1%85%D0%BE%D0%BC%D1%8F%D0%BA%D0%B8%20%D0%B2%D0%B5%D0%B4%D1%83%D1%82%20%D0%BD%D0%BE%D1%87%D0%BD%D0%BE%D0%B9,%D0%B8%D0%B7%D0%B1%D0%B5%D0%B3%D0%B0%D1%82%D1%8C%20%D1%81%D1%82%D0%BE%D0%BB%D0%BA%D0%BD%D0%BE%D0%B2%D0%B5%D0%BD%D0%B8%D1%8F%20%D1%81%20%D0%B6%D0%B0%D1%80%D0%BA%D0%B8%D0%BC%20%D0%BA%D0%BB%D0%B8%D0%BC%D0%B0%D1%82%D0%BE%D0%BC.')
     keyboard_inline2.add( but_inline3, but_inline4)
     return keyboard_inline2
+
+def get_key_inline3():
+    keyboard_inline3 = InlineKeyboardMarkup(row_width=3)
+    button5 = InlineKeyboardButton('Отправь злого кота', url='https://i.pinimg.com/736x/01/60/09/016009b63930c8862755f9fa6e3bf511.jpg', callback_data='cat')
+    button6 = InlineKeyboardButton('Отправь шутку', callback_data='lol')
+    button7 = InlineKeyboardButton('Ыъыъы', callback_data='ioi')
+    button8 = InlineKeyboardButton('>>', callback_data='go_to_1')
+    keyboard_inline3.add(button5, button6, button7, button8)
+    return keyboard_inline3
+
+def get_key_inline4():
+    keyboard_inline4 = InlineKeyboardMarkup(row_width=3)
+    button9 = InlineKeyboardButton('Отправь злого пса', url='https://i.pinimg.com/736x/03/1c/4b/031c4b2665938050584f83db949e8dc6.jpg')
+    button10 = InlineKeyboardButton('Отправь злого хомяка', url='https://i.pinimg.com/736x/47/ce/f2/47cef2d8c4866bb7bc90f0ccd1aa5d13.jpg')
+    button11 = InlineKeyboardButton('Отправь...', callback_data='send')
+    button12 = InlineKeyboardButton('время', callback_data='time')
+    button13 = InlineKeyboardButton('отправь случайное число', callback_data='send_random')
+    button14 = InlineKeyboardButton('<<', callback_data='go_to_2')
+    keyboard_inline4.add(button9, button10, button11, button12, button13, button14)
+    return keyboard_inline4
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
 from keyboard.keyboards import  get_keyboard1, get_keyboard2
-from keyboard.key_inline import get_keyboard_inline, get_key_inline2
-
+from keyboard.key_inline import get_keyboard_inline, get_key_inline2, get_key_inline3, get_key_inline4
+from datetime import datetime
+import random
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
@@ -21,7 +22,42 @@ async def set_commands(bot: Bot):
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    await message.answer('привет, я неформал', reply_markup= get_keyboard1())
+    await message.answer('привет, я неформал', reply_markup= get_key_inline3() )
+
+@dp.callback_query_handler(lambda c: c.data == 'go_to_1')
+async def go_to_1(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text('>>', reply_markup=get_key_inline4())
+
+
+@dp.callback_query_handler(lambda c: c.data == 'lol')
+async def lol(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f'Я не клоун')
+
+
+@dp.callback_query_handler(lambda c: c.data == 'ioi')
+async def ioi(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f'ъуъуъуъ')
+
+@dp.callback_query_handler(lambda c: c.data == 'go_to_2')
+async def go_to_2(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text('<<', reply_markup=get_key_inline3())
+
+@dp.callback_query_handler(lambda c: c.data == 'send')
+async def send(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f'Что за зоопарк вы тут развели?')
+
+@dp.callback_query_handler(lambda c: c.data == 'send_random')
+async def random_number(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f'Случайное число: {random_number}')
+random_number = random.randint(1, 100)
+
+@dp.callback_query_handler(lambda c: c.data == 'time')
+async def time(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(f'Сейчас: {time}')
+time = datetime.now().strftime("%H:%M:%S")
+
+
+
 
 @dp.message_handler(lambda message: message.text == 'Отправь злого кота')
 async def button_1_click(message: types.Message):
@@ -71,9 +107,6 @@ async def start(message: types.Message):
 async def start(message: types.Message):
     await message.answer('- ˕ •マ')
 
-@dp.message_handler(commands='hehe')
-async def start(message: types.Message):
-    await message.reply('Что может быть хуже, чем мальчика, которого сбила машина? Холо.......')
 
 @dp.message_handler()
 async def echo(message: types.Message):
